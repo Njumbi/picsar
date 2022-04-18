99e18268a50b747a2b11f58bb59d79711bdedcbb
Florence Njumbi <wambuinjumbi@gmail.com>
2022-04-18 14:02:08 +0300

    refactor

---
+++ app/build/generated/source/kapt/debug/com/example/picsar/picsar/ui/activities/WallpaperActivity_GeneratedInjector.java
@@ -0,0 +1,15 @@
+package com.example.picsar.picsar.ui.activities;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ActivityComponent;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.internal.GeneratedEntryPoint;
+
+@OriginatingElement(
+    topLevelClass = WallpaperActivity.class
+)
+@GeneratedEntryPoint
+@InstallIn(ActivityComponent.class)
+public interface WallpaperActivity_GeneratedInjector {
+  void injectWallpaperActivity(WallpaperActivity wallpaperActivity);
+}
